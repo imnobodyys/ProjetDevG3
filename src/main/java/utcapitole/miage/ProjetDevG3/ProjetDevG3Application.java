6a61ad3a332c3c868c015dc1bb356e24fe06df4f
ziming <knowszm@icloud.com>
2025-05-12 09:58:00 +0200

    first commit

---
+++ src/main/java/utcapitole/miage/ProjetDevG3/ProjetDevG3Application.java
@@ -0,0 +1,13 @@
+package utcapitole.miage.ProjetDevG3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDevG3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDevG3Application.class, args);
+	}
+
+}
---
+++ src/test/java/utcapitole/miage/ProjetDevG3/ProjetDevG3ApplicationTests.java
@@ -0,0 +1,13 @@
+package utcapitole.miage.ProjetDevG3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDevG3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
