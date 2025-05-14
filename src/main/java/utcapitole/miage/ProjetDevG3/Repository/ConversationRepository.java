21e01feecfda03068423082881787793ecd475c7
ziming <knowszm@icloud.com>
2025-05-14 14:57:25 +0200

    essayer

--- src/main/java/utcapitole/miage/ProjetDevG3/Repository/ConversationRepository.java
+++ src/main/java/utcapitole/miage/ProjetDevG3/Repository/ConversationRepository.java
@@ -1,15 +1,13 @@
 package utcapitole.miage.projetDevG3.Repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import utcapitole.miage.projetDevG3.model.Conversation;
 
-
 @Repository
 public interface ConversationRepository extends JpaRepository<Conversation, Long> {
     // Méthode pour trouver une conversation par son ID
     Conversation findById(long id);
-    
+
 }
