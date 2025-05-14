4da155c2e1d8d78fe1e40710baec7ce37f4570ac
Wenfriedlefou <fabioanifrani@gmail.com>
2025-05-14 15:10:00 +0000

    supppression controller

--- src/main/java/utcapitole/miage/ProjetDevG3/Repository/ConversationRepository.java
+++ src/main/java/utcapitole/miage/ProjetDevG3/Repository/ConversationRepository.java
@@ -9,11 +9,5 @@ import utcapitole.miage.projetDevG3.model.Conversation;
 public interface ConversationRepository extends JpaRepository<Conversation, Long> {
     // Méthode pour trouver une conversation par son ID
     Conversation findById(long id);
-<<<<<<< HEAD
-    // Méthode pour trouver toutes les conversations d'un utilisateur
-    List<Conversation> findByUtilisateur(Utilisateur utilisateur);
-    
-=======
->>>>>>> 21e01feecfda03068423082881787793ecd475c7
 
 }
