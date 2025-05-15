f23d26c7cd0df7b91a82b31393c542bc2ffa7ad8
BAI YUEJIE <baiyuejiefr@gmail.com>
2025-05-15 10:48:33 +0200

    ajouter dependency de security

--- src/main/java/utcapitole/miage/ProjetDevG3/Service/UtilisateurService.java
+++ src/main/java/utcapitole/miage/ProjetDevG3/Service/UtilisateurService.java
@@ -1,8 +1,12 @@
 package utcapitole.miage.projetDevG3.Service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import utcapitole.miage.projetDevG3.Repository.UtilisateurRepository;
+import utcapitole.miage.projetDevG3.model.Utilisateur;
 
 /**
  * Service pour la gestion des utilisateurs.
@@ -12,5 +16,28 @@ import utcapitole.miage.projetDevG3.Repository.UtilisateurRepository;
 public class UtilisateurService {
     @Autowired
     private UtilisateurRepository utilisateurRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    /**
+     * US01
+     * Crée un nouvel utilisateur avec cryptage du mot de passe
+     * @param utilisateur Objet utilisateur à créer
+     * @return Utilisateur créé
+     * @throws IllegalArgumentException Si l'email existe déjà
+     */
+    public Utilisateur creerUtilisateur(Utilisateur utilisateur){
+        if(utilisateurRepository.findByEmail(utilisateur.getEmail()).isPresent()){
+            throw new IllegalArgumentException("Cet email est déjà utilisé !");
+        }
+
+        utilisateur.setDtInscription(LocalDateTime.now());
+
+        utilisateur.setMdp(passwordEncoder.encode(utilisateur.getMdp()));
+
+        return utilisateurRepository.save(utilisateur);
+        
+    }
 
 }
