3f0d8822111b41a351c8e1d96fd206d41261592c
ziming <knowszm@icloud.com>
2025-05-15 10:42:47 +0200

    page1

--- src/main/java/utcapitole/miage/ProjetDevG3/Controller/UtilisateurController.java
+++ src/main/java/utcapitole/miage/ProjetDevG3/Controller/UtilisateurController.java
@@ -1,5 +1,38 @@
 package utcapitole.miage.projetDevG3.Controller;
 
+import java.util.List;
+import org.springframework.ui.Model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import utcapitole.miage.projetDevG3.Service.UtilisateurService;
+import utcapitole.miage.projetDevG3.model.Utilisateur;
+
+@RestController
+@RequestMapping("/api/utilisateurs")
 public class UtilisateurController {
 
+    @Autowired
+    private UtilisateurService utilisateurService;
+
+    @GetMapping("/search")
+    public String searchUtilisateurs(@RequestParam(value = "q", required = false) String keyword, Model model) {
+        List<Utilisateur> resultats = null;
+
+        // si le utilisateur saisi le keword on commence recherche
+        if (keyword != null && !keyword.isEmpty()) {
+            resultats = utilisateurService.rechercher(keyword);
+        }
+
+        // transmis key word et resultat
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("resultats", resultats);
+
+        // rentrer page search
+        return "search";
+    }
 }
--- src/main/java/utcapitole/miage/ProjetDevG3/Service/UtilisateurService.java
+++ src/main/java/utcapitole/miage/ProjetDevG3/Service/UtilisateurService.java
@@ -1,16 +1,23 @@
 package utcapitole.miage.projetDevG3.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import utcapitole.miage.projetDevG3.Repository.UtilisateurRepository;
+import utcapitole.miage.projetDevG3.model.Utilisateur;
 
 /**
  * Service pour la gestion des utilisateurs.
- * Administre les opérations CRUD et la logique métier liée aux comptes utilisateurs.
+ * Administre les opérations CRUD et la logique métier liée aux comptes
+ * utilisateurs.
  */
 @Service
 public class UtilisateurService {
     @Autowired
     private UtilisateurRepository utilisateurRepository;
 
+    public List<Utilisateur> rechercher(String keyword) {
+        return utilisateurRepository.searchByKeyword(keyword);
+    }
 }
