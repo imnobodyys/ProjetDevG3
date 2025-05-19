1d21e525498f128d993b2aa8f5b94ac1d53e1cea
BAI YUEJIE <baiyuejiefr@gmail.com>
2025-05-19 16:45:41 +0200

    controller

--- src/main/java/utcapitole/miage/ProjetDevG3/Service/EvenementService.java
+++ src/main/java/utcapitole/miage/ProjetDevG3/Service/EvenementService.java
@@ -1,7 +1,5 @@
 package utcapitole.miage.projetDevG3.Service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import utcapitole.miage.projetDevG3.Repository.EvenementRepository;
