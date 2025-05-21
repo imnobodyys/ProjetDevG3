dd5a35697ea4eb74407ed249c1a462386d142c31
Wenfriedlefou <fabioanifrani@gmail.com>
2025-05-21 11:20:34 +0000

    accueil

--- src/main/java/utcapitole/miage/projetdevg3/controller/AccueilController.java
+++ src/main/java/utcapitole/miage/projetdevg3/controller/AccueilController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+import lombok.RequiredArgsConstructor;
 import utcapitole.miage.projetdevg3.model.Utilisateur;
 import utcapitole.miage.projetdevg3.service.UtilisateurService;
 
